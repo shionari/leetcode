9c521b765ca6696935563529ae0129a327813c2c
shionari <549865368@qq.com>
2018-05-11 08:59:55 +0800

    修复错误

--- src/main/java/algorithm/simple/array/Rotate.java
+++ src/main/java/algorithm/simple/array/Rotate.java
@@ -14,7 +14,7 @@ public class Rotate {
         int temp = 0;
         for (int i = 0; i < k; i++) {
             temp = nums[nums.length - 1];
-            for (int j = nums.length - 1; j > 0; j++) {
+            for (int j = nums.length - 1; j > 0; j--) {
                 nums[j] = nums[j - 1];
             }
             nums[0] = temp;
